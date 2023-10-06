a2f679d98d8e7f331dcf20b4764aa370fac5aebb
banuprakash92 <banuprakash.banu@gmail.com>
2023-10-05 21:42:23 -0400

    restaurants placeholders

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,15 @@ const userController = require('../controllers/user');
 
 const router = express.Router();
 
+//Signup and Login routes
 router.post("/user/create", userController.createUser);
 router.get('/user/name/:first_name', userController.findUserByFirstName);
 router.get('/user', userController.getAllUsers);
 router.get('/user/id/:_id', userController.getUserById);
 router.post('/user/login', userController.login);
+
+//Forgot password routes
+router.post("/user/forgot-password", userController.forgotPassword);
+router.post("/user/reset-password", userController.resetPassword);
 
 module.exports = router;
