5a9ca5fd714264be16ca3318e89703286eeee885
TomDiaz-Piedra <46943699+TomDiaz-Piedra@users.noreply.github.com>
2023-10-19 13:59:35 -0400

    Added OrderModel

---
+++ src/models/orderModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+// Define the Order Schema
+const orderSchema = new mongoose.Schema({
+
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // Foreign Key reference to Users
+        required: true,
+    },
+    restaurant_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Restaurant', // Foreign Key reference to Restaurants
+        required: true,
+    },
+    order_status: {
+        type: String,
+        enum: ['Placed', 'In Progress', 'Completed', 'Canceled'],
+        required: true,
+    },
+    order_date: {
+        type: Date,
+        default: Date.now,
+    },
+    delivery_address: String,
+    total_price: {
+        type: Number,
+        required: true,
+    },
+    payment_info_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'PaymentInfo', // Foreign Key reference to PaymentInfo
+    },
+});
+
+// Create and export the Order model
+const Order = mongoose.model('Order', orderSchema, 'orders');
+
+module.exports = Order;
